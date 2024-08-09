ce697d18d46f5a306298c971e10112244f680bb1
phungnguyendev <phungnguyengarment.dev@gmail.com>
2024-08-09 16:38:11 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,4 @@
-import compression from 'compression'
 import express from 'express'
-import helmet from 'helmet'
-import morgan from 'morgan'
-import { responseEnhancer } from '~/middleware/express-formatter/index'
 import routes from '~/routes/index'
 import errorHandler from './api/middleware/errorHandler'
 import sequelize from './api/models'
@@ -12,14 +8,14 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 // (helmet) helps secure Express apps by setting HTTP response headers.
-app.use(morgan('dev'))
-app.use(helmet())
-app.use(compression())
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+// app.use(morgan('dev'))
+// app.use(helmet())
+// app.use(compression())
+// app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
 // (morgan) HTTP request logger middleware for node.js
 // (cors) Provide some options Headers for accept others localhost to allow request
 // Handle custom formatter response express (middleware)
-app.use(responseEnhancer())
+// app.use(responseEnhancer())
 app.use('/api', routes)
 
 app.use(errorHandler)
